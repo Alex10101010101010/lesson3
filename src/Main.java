387038c7835c695bdc0cc6d90bcab09c7d81473f
Alex10101010101010 <alex2010-201010@yandex.ru>
2022-06-22 23:47:37 +0300

    Вводим аргументы через консоль для вычислений и получаем результат

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        Test test = new Test();
-        test.method(10, 25);
+public class Main {
+
+    private static final Test test = new Test();
+    private static int sum1 = 0;
+    private static int sum2 = 0;
+
+    public static void main(String[] args) {
+        inputSum();
+
+        test.method(sum1, sum2);
+    }
+
+    private static void inputSum() {
+        Scanner console1 = new Scanner(System.in);
+        System.out.print("введите первое число: ");
+        sum1 = console1.nextInt();
+
+        Scanner console2 = new Scanner(System.in);
+        System.out.print("введите второе число: ");
+        sum2 = console2.nextInt();
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@ public class Test {
 
     public void method(int a, int b) {
 
-        System.out.println(a + b);
+        System.err.println("Мы сложили Ваши числа и получили: " + (a + b));
     }
 }
