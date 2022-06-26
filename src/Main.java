d896b999d24de09ace932217ff68d86453c30c86
Alex10101010101010 <alex2010-201010@yandex.ru>
2022-06-26 16:59:08 +0300

    Комментарии по коду

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
 
 public class Main {
 
-    static MyRandom myRandom = new MyRandom();
-    static MyScanner myScanner = new MyScanner();
-    static int numbersRandom = myRandom.numbersRandom();
+    static MyRandom myRandom = new MyRandom();//создание экземпляра класса MyRandom
+    static MyScanner myScanner = new MyScanner();//создание экземпляра класса MyScanner
+    static int numbersRandom = myRandom.numbersRandom();//создали переменную и инициализировали ее
 
+    //это главный метод, где запускается программа
     public static void main(String[] args) {
 
-        System.out.println("ПРОВЕРКА - Компьютер загадал число: " + numbersRandom);
+        System.out.println("ПРОВЕРКА - Компьютер загадал число: " + numbersRandom);//это просто для проверки того, что компьютер загадал
 
-        resultMethod();
+        resultMethod();//Вызов метода
     }
 
+    //невозвращаемый метод. Сравнивание загадонного числа и введенного через консоль числа
     private static void resultMethod() {
 
-        System.out.print("Компьютер загадал число от 0 до 3, угодай число: ");
+        System.out.print("Компьютер загадал число от 0 до 3, угадай число: ");//Вывод в консоль
 
-        int myNumberScanner = myScanner.myNumberScanner();
+        int myNumberScanner = myScanner.myNumberScanner();//создали переменную и инициализировали ее
 
-        if (numbersRandom == myNumberScanner) {
-            System.err.println("УРА Вы угодали, это число: " + numbersRandom);
+        //
+        if (numbersRandom == myNumberScanner) {//сравнение двух переменных (сравниваем числа)
+            System.err.println("УРА Вы угодали, это число: " + numbersRandom);//Победа, выводим сообщение в консоль
         } else {
-            System.out.println("Вы не угодали, компьютер загодал число: " + numbersRandom + "\nПопробуйте в другой раз!!");
+            //Проигрыш, выводим сообщение в консоль
+            System.out.println("Вы не угадали, компьютер загадал число: " + numbersRandom + "\nПопробуйте в другой раз!!!");
         }
     }
 }
