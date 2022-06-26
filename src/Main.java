6a52607323a28db37fd0ef4d1827b225c453048a
Alex10101010101010 <alex2010-201010@yandex.ru>
2022-06-26 18:00:09 +0300

    Угадайка чисел с циклом while. Пока не угадаешь число, программа не закончит работу. Каждый цикл, новое число

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 
 public class Main {
-
     static MyRandom myRandom = new MyRandom();//создание экземпляра класса MyRandom
     static MyScanner myScanner = new MyScanner();//создание экземпляра класса MyScanner
-    static int numbersRandom = myRandom.numbersRandom();//создали переменную и инициализировали ее
 
     //это главный метод, где запускается программа
     public static void main(String[] args) {
-
-        System.out.println("ПРОВЕРКА - Компьютер загадал число: " + numbersRandom);//это просто для проверки того, что компьютер загадал
 
         resultMethod();//Вызов метода
     }
@@ -16,16 +12,22 @@ public class Main {
     //невозвращаемый метод. Сравнивание загадонного числа и введенного через консоль числа
     private static void resultMethod() {
 
-        System.out.print("Компьютер загадал число от 0 до 3, угадай число: ");//Вывод в консоль
+        while (true) {
+            int numbersRandom = myRandom.numbersRandom();//создали переменную и инициализировали ее
 
-        int myNumberScanner = myScanner.myNumberScanner();//создали переменную и инициализировали ее
+            System.out.println("ПРОВЕРКА - Компьютер загадал число: " + numbersRandom);//это просто для проверки того, что компьютер загадал
 
-        //
-        if (numbersRandom == myNumberScanner) {//сравнение двух переменных (сравниваем числа)
-            System.err.println("УРА Вы угодали, это число: " + numbersRandom);//Победа, выводим сообщение в консоль
-        } else {
-            //Проигрыш, выводим сообщение в консоль
-            System.out.println("Вы не угадали, компьютер загадал число: " + numbersRandom + "\nПопробуйте в другой раз!!!");
+            System.out.print("Компьютер загадал число от 0 до 3, угадай число: ");//Вывод в консоль
+
+            int myNumberScanner = myScanner.myNumberScanner();//создали переменную и инициализировали ее
+
+            if (numbersRandom == myNumberScanner) {//сравнение двух переменных (сравниваем числа)
+                System.err.println("УРА Вы угодали, это число: " + numbersRandom);//Победа, выводим сообщение в консоль
+                break;
+            } else {
+                //Проигрыш, выводим сообщение в консоль
+                System.out.println("Вы не угадали, компьютер загадал число: " + numbersRandom + "\nПопробуйте еще раз!!!\n");
+            }
         }
     }
 }
