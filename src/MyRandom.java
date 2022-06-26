838eb902d3aa6af771d177bf7ab53e7e741e16d0
Alex10101010101010 <alex2010-201010@yandex.ru>
2022-06-26 16:59:08 +0300

    Комментарии по коду

--- src/MyRandom.java
+++ src/MyRandom.java
@@ -1,14 +1,18 @@
 import java.util.Random;
 
+/**
+ * этот класс генерирует случайные числа
+ */
+
 public class MyRandom {
 
     private static final Random random = new Random();//создали экземпляр класса
 
-    private static final int max = 3;//завели глобальную переменную
+    private static final int MAX = 3;//завели глобальную переменную (это константа)
 //    private static final int min = -100;//завели глобальную переменную
 
-    //создали метод с вычислениями случайных сисел
+    //создали возвращаемый метод случайных чисел
     public int numbersRandom() {
-        return random.nextInt(max);
+        return random.nextInt(MAX);
     }
 }
--- src/MyScanner.java
+++ src/MyScanner.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 
+/**
+ * этот класс считывает введенное значения с консоли
+ */
+
 public class MyScanner {
 
-    private final Scanner myNumber = new Scanner(System.in);
-    int result;
+    private final Scanner myNumber = new Scanner(System.in);//создание экземпляра класса Scanner
+
+    //возвращаемый метод где присваиваем введенное значение с консоли переменной result
     public int myNumberScanner() {
-        result = myNumber.nextInt();
+        int result = myNumber.nextInt();
         return result;
     }
 }
