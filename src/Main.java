07bff36670989ff191c6f359072b5fe17e2395a7
Alex10101010101010 <alex2010-201010@yandex.ru>
2022-07-03 15:43:02 +0300

    Пример ветвления с if

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
     static int number = 0;
     public static void main(String[] args) {
 
-        if (number >= 0) {
-            System.out.println("Номер больше нуля или равно нулю");
+        if (number > 0) {
+            System.out.println("Номер больше нуля");
+        } else if (number < 0) {
+            System.out.println("Номер меньше нуля");
         } else {
-            System.out.println("Номер не равен нулю");
+            System.out.println("Номер равен нулю");
         }
     }
 }
