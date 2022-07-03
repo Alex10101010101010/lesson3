966a8c6900b79840950e274f3759e7b38ff1bff5
Alex10101010101010 <alex2010-201010@yandex.ru>
2022-07-03 15:30:30 +0300

    Пример наследования от класса (расширяем характеристики класса) и обращение к переменной через класс с использованием get, set.

---
+++ src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+
+    private int speed;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Fox fox = new Fox();
+        fox.setSpeed(50);
+        System.out.println(fox.getSpeed());
+    }
 }
