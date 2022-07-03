a7bd020bfb7d410b109fce05ce32db2e637a862f
Alex10101010101010 <alex2010-201010@yandex.ru>
2022-07-03 15:47:54 +0300

    Пример упращения написания условий при использовании if

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ public class Main {
     static int number = 0;
     public static void main(String[] args) {
 
-        if (number > 0) {
-            System.out.println("Номер больше нуля");
-        } else if (number < 0) {
-            System.out.println("Номер меньше нуля");
+        if (number >= 0) {
+            System.out.println("Номер больше нуля или равно нулю");
         } else {
-            System.out.println("Номер равен нулю");
+            System.out.println("Номер не равен нулю");
         }
+
+
+        //Пример написания альтернативного кода (смотри выше)
+        String alternative = number >= 0 ? "Номер больше нуля или равно нулю" : "Номер не равен нулю";
+        System.out.println(alternative);
     }
 }
