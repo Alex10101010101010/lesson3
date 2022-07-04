4a8da17c264afbd2d2e2df1a8204aae53110c65e
Alex10101010101010 <alex2010-201010@yandex.ru>
2022-07-04 14:05:31 +0300

    Цыкл for

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     static int number = 0;
+
     public static void main(String[] args) {
 
         if (number >= 0) {
@@ -9,9 +10,12 @@ public class Main {
             System.out.println("Номер не равен нулю");
         }
 
-
         //Пример написания альтернативного кода (смотри выше)
         String alternative = number >= 0 ? "Номер больше нуля или равно нулю" : "Номер не равен нулю";
         System.out.println(alternative);
+
+        for (int i = 30; i < 50; i++) {
+            System.out.println("ПРИВЕТ ! " + i);
+        }
     }
 }
