a55d940a7c4ea0f9798bf5a9a99283a84abb986b
Alex10101010101010 <alex2010-201010@yandex.ru>
2022-07-15 16:58:58 +0300

    implements

--- src/Barn.java
+++ src/Barn.java
@@ -1,2 +1,9 @@
-public class Barn extends Building {
+public class Barn extends Building implements Warm {
+
+
+    @Override
+    public void warm() {
+        System.out.println("I am warm barn .......");
+
+    }
 }
--- src/House.java
+++ src/House.java
@@ -1,7 +1,12 @@
 /**
  * final class - от этого класса нельзя наследовоться
  */
-public final class House extends Building {
+public final class House extends Building implements Warm {
 
     int floor;
+
+    @Override
+    public void warm() {
+        System.out.println("I am warn farm ............");
+    }
 }
